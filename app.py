552613b124c12e17198b82a5be19e5d7004a6ccc
SimpleSenpai12 <70236472+simplesentai@users.noreply.github.com>
2021-11-12 21:45:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ secret_code = open("config.txt", 'r').read()
 client = NotionClient(token_v2=secret_code)
 
 # Replace this URL with the URL of the page you want to edit
-page = client.get_block("https://www.notion.so/simplesenpai/test-page-for-notion-python-6c9097ffa4c147e2a3ef3afc43a779d9")
+title = client.get_block("https://www.notion.so/simplesenpai/test-page-for-notion-python-6c9097ffa4c147e2a3ef3afc43a779d9")
+content = client.get_block("https://www.notion.so/simplesenpai/test-page-for-notion-python-6c9097ffa4c147e2a3ef3afc43a779d9#2f8be5503d9b4e108cebd76d43f558cb")
+
 
 print("Welcome to NotionPy!")
 
-print("Current Editing Page:", page.title)
+print("Current Editing Page:", title)
 
 choice = ""
 
@@ -22,15 +24,15 @@ while choice.lower() != "exit":
         print("content read/change - Reads / changes the content of the working page!")
         print("exit - Exits the program!")
     elif choice.lower() == "title read":
-        print("The title of the page is:", page.title)
+        print("The title of the page is:", title)
     elif choice.lower() == "title change":
         new_title = input("What would you like to change the title to? ")
-        page.title = new_title
+        title = new_title
     elif choice.lower() == "content read":
-        print("The content of the page is:", page.content)
+        print("The content of the page is:", content)
     elif choice.lower() == "content change":
         new_content = input("What would you like to change the content to? ")
-        page.content = new_content
+        content = new_content
     elif choice.lower() == "exit":
         print("Exiting the program...")
         break
